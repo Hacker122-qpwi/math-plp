cc1984a77a52fd7b51a78b5bb4314e823ee82fc9
Hacker122-qpwi <yonghouvh@gmail.com>
2025-06-09 22:17:57 +0700

    Add files via upload

---
+++ menu/javascript/index.js
@@ -0,0 +1,30 @@
+function btn1() {
+    window.location.href = "/menu/html/home.html";
+}
+function btn2() {
+    window.location.href = "/menu/html/home2.html";
+}
+function btn3() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn4() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn5() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn6() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn7() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn8() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn9() {
+    window.location.href = "/menu/html/home3.html";
+}
+function btn10() {
+    window.location.href = "/menu/html/home3.html";
+}
