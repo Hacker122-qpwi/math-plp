e683bf7a74f606d97dc07fbcbbe064f9a2c564dd
Hacker122-qpwi <yonghouvh@gmail.com>
2025-06-10 10:10:26 +0700

    Add files via upload

---
+++ menu/javascript/grade.js
@@ -0,0 +1,9 @@
+function link1(){
+    window.location.href = "https://khmer-bice.vercel.app/";
+}
+function link2(){
+    window.location.href = "https://math1-zeta.vercel.app/";
+}
+function link3(){
+    window.location.href = "";
+}
---
+++ menu/javascript/script.js
@@ -0,0 +1,18 @@
+function btn1() {
+    window.location.href = "/menu/html/grade1.html";
+}
+function btn2() {
+    window.location.href = "/menu/html/grade2.html";
+}
+function btn3() {
+    window.location.href = "/menu/html/grade3.html";
+}
+function btn4() {
+    window.location.href = "/menu/html/grade4.html";
+}
+function btn5() {
+    window.location.href = "/menu/html/grade5.html";
+}
+function btn6() {
+    window.location.href = "/menu/html/grade6.html";
+}
